21141ede2281e1af4ac1412352f58647da4ee23a
afeiyoo <afeiyoo@gmail.com>
2024-08-10 11:45:31 +0800

    [chore] 新增clangformat格式化配置文件

--- project/main.cpp
+++ project/main.cpp
@@ -1,13 +1,13 @@
+#include <QApplication>
+
 #include "src/bend/man/mancloud.h"
 #include "src/bend/man/mandb.h"
 #include "src/config/loggerproxy.h"
 #include "src/fend/uimain/uimain.h"
 #include "src/helper/filehelper.h"
 #include "src/plugins/manplugin.h"
-#include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     MP->installPlugins(argc, argv);
